3a592f3e028d90b6aa3ad30974d519d1a0b3a597
Daniel Frech <dfrechdev@gmail.com>
2019-04-06 13:51:10 +0200

    corrected departures.js

--- src/departures.js
+++ src/departures.js
@@ -1,9 +1,18 @@
-// services to handles the request details and to create, add and remove flight cards
-// const apiService = new FlightAPICallbackService("http://localhost:3000");
 const apiService = new FlightAPIPromiseService("http://localhost:3000");
 const cardService = new FlightCardService("#flights-container");
 
-// main function responsible for loading the flights
 const loadFlights = async () => {
-    loadFlightsWithAsyncAwaitParallel(apiService, cardService);
+    apiService.getFlights(flights => {
+        flights.forEach(flight => {
+            let flightCard = new FlightCard(flight);
+            apiService.getAirline(flight, airline => {
+                apiService.getAircraft(flight, aircraft => {
+                    apiService.getDestination(flight, destination => {
+                        flightCard.setDetails(airline, aircraft, destination);
+                        cardService.addCardToPage(flightCard);
+                    });
+                });
+            });
+        });
+    });
 };
