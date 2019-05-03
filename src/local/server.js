2e8c2ceab91520a390f3e58e0af486750248b805
Daniel Frech <dfrechdev@gmail.com>
2019-05-03 14:12:24 +0200

    refactored local folder structure

--- src/local/server.js
+++ src/local/server.js
@@ -2,8 +2,10 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-// define static routes
-app.use("/js", express.static(__dirname + "/js/"));
+// add static routes
+app.use("/departures.js", express.static(__dirname + "/departures.js"));
+app.use("/services", express.static(__dirname + "/services/"));
+app.use("/entities", express.static(__dirname + "/entities/"));
 app.use("/styles", express.static(__dirname + "/styles/"));
 app.use("/static", express.static(__dirname + "/static/"));
 app.use(express.static("app"));
