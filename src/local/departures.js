db99f770a8f13c8c41cbc65908ce47ab340f5a87
Daniel Frech <dfrechdev@gmail.com>
2019-05-07 00:24:12 +0200

    added new apex export

--- src/local/departures.js
+++ src/local/departures.js
@@ -1,7 +1,3 @@
-/**
- * @description
- * loading flights using parallel callback functions
- */
 const apiService = new FlightAPICallbackService("http://localhost:3000");
 const cardService = new FlightCardService("#flights-container");
 
@@ -9,18 +5,8 @@ const loadFlights = () => {
     apiService.getFlights(flights => {
         flights.forEach(flight => {
             let flightCard = new FlightCard(flight);
-            apiService.getAirline(flight, airline => {
-                flightCard.setAirline(airline);
-                cardService.addCardIfAllIsSet(flightCard);
-            });
-            apiService.getAircraft(flight, aircraft => {
-                flightCard.setAircraft(aircraft);
-                cardService.addCardIfAllIsSet(flightCard);
-            });
-            apiService.getDestination(flight, destination => {
-                flightCard.setDestination(destination);
-                cardService.addCardIfAllIsSet(flightCard);
-            });
+            // flight details
+            cardService.addCardToPage(flightCard);
         });
     });
 };
